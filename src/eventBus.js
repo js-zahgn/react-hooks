0a6960b89bfdc98b2535920d6fe228bbdf85c8da
张怀忠 <zhanghuaizhong@vchangyi.com>
2020-04-24 10:13:23 +0800

    更新EventBus

--- src/eventBus.js
+++ src/eventBus.js
@@ -5,25 +5,17 @@ function EventBus() {
 EventBus.prototype = {
   $on: function (fnName, fn) {
     if (this.fnQueue.hasOwnProperty(fnName)) {
-      if (typeof this.fnQueue[fnName] === "function") {
-        this.fnQueue[fnName] = [this.fnQueue[fnName], fn];
-      } else {
-        this.fnQueue[fnName] = [...this.fnQueue[fnName], fn];
-      }
+      this.fnQueue[fnName] = [...this.fnQueue[fnName], fn];
     } else {
-      this.fnQueue[fnName] = fn;
+      this.fnQueue[fnName] = [fn];
     }
   },
   $once: function (fnName, fn) {
-    this.fnQueue[fnName] = fn;
+    this.fnQueue[fnName] = [fn];
   },
   $emit: function (fnName, params) {
     if (!this.fnQueue.hasOwnProperty(fnName)) return;
-    if (typeof this.fnQueue[fnName] === "function") {
-      this.fnQueue[fnName](params);
-    } else {
-      this.fnQueue[fnName].map(fn => fn(params));
-    }
+    this.fnQueue[fnName].map(fn => fn(params));
   },
   $off: function (fnName) {
     if (!this.fnQueue.hasOwnProperty(fnName)) return;
